3cdb801ad681be1f4747326164cb23577422a9af
Tiago Filipe Oliveira <tiagoliv@c1r16s13.42lisboa.com>
2024-06-06 13:57:42 +0100

    small tweak

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stdlib.h>
 #include <cctype>
 
 int main(int argc, char **argv)
@@ -7,13 +6,13 @@ int main(int argc, char **argv)
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return EXIT_SUCCESS;
+		return 0;
 	}
 	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << (char)std::toupper((unsigned char)argv[i][j]); // Correct use of std::toupper
+			std::cout << (char)std::toupper((unsigned char)argv[i][j]);
 	}
 	std::cout << std::endl;
-	return EXIT_SUCCESS;
+	return 0;
 }
