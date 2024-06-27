9674622ad56b7a7fd846fbcc7e8a02fad26e3d72
Tiago Filipe Oliveira <tiagoliv@c2r2s3.42lisboa.com>
2024-06-27 17:56:45 +0100

    ex01

---
+++ Module07/ex01/includes/iter.hpp
@@ -0,0 +1,24 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+typedef unsigned int uint;
+
+template<typename T>
+void    iter(const T* array, uint len, void (*func)(T const &element)) {
+    if (array == NULL || func == NULL || len == 0)
+        return ;
+    for (uint i = 0; i < len; i++) {
+        func(array[i]);
+    }
+}
+
+template<typename T>
+void    iter(T* array, uint len, void (*func)(T &element)) {
+    if (array == NULL || func == NULL || len == 0)
+        return ;
+    for (uint i = 0; i < len; i++) {
+        func(array[i]);
+    }
+}
+
+#endif // ITER_HPP
