1944d9df1dc8a197b1b768a0f3b8cb60a7412245
Tiago-0liveira <tfgoliveira@gmail.com>
2024-09-17 23:34:28 +0100

    started cpp09-ex00

---
+++ Module09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+//
+// Created by tfgol on 17/09/2024.
+//
+
+#ifndef EX00_BITCOINEXCHANGE_HPP
+#define EX00_BITCOINEXCHANGE_HPP
+
+
+
+class BitcoinExchange {
+
+};
+
+
+
+#endif //EX00_BITCOINEXCHANGE_HPP
