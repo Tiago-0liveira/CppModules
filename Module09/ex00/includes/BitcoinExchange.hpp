362951601449d889db3915e7fc383f7c6cf8ab92
Tiago-0liveira <tfgoliveira@gmail.com>
2024-09-17 23:35:15 +0100

    started cpp09-ex00

--- Module09/ex00/includes/BitcoinExchange.hpp
+++ Module09/ex00/includes/BitcoinExchange.hpp
@@ -1,16 +1,43 @@
-//
-// Created by tfgol on 17/09/2024.
-//
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
 
-#ifndef EX00_BITCOINEXCHANGE_HPP
-#define EX00_BITCOINEXCHANGE_HPP
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <fstream>
 
-
-
-class BitcoinExchange {
-
+class BitcoinExchange
+{
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &other);
+	BitcoinExchange &operator=(const BitcoinExchange &other);
+	
+	float applyExchangeRate(const std::string &date, float amount) const;
+	
+	void applyRatesToFile(const std::string &inputFile);
+	bool validDate(const std::string &date) const;
+	bool validValue(float value) const;
+	
+	struct Record
+	{
+		Record(const std::string &d, float v);
+		const std::string &date;
+		float value;
+	};
+private:
+	void loadDatabase(const std::string &database);
+	
+	Record parseLine(const std::string &line, bool &error, char sep, bool verbose = true);
+	Record parseDatabaseLine(const std::string &line, bool &error, bool verbose = true);
+	Record parseInputLine(const std::string &line, bool &error, bool verbose = true);
+	
+	std::map<std::string, float> m_data;
+	static const std::string &databasePath;
 };
 
 
-
-#endif //EX00_BITCOINEXCHANGE_HPP
+#endif //BITCOINEXCHANGE_HPP
--- Module09/ex00/src/classes/BitcoinExchange.cpp
+++ Module09/ex00/src/classes/BitcoinExchange.cpp
@@ -1,1 +1,154 @@
-#include "BitcoinExchange.hpp"
+#include <BitcoinExchange.hpp>
+#include <sstream>
+
+const std::string &BitcoinExchange::databasePath = "data.csv";
+
+BitcoinExchange::Record::Record(const std::string &d, float v) : date(d), value(v) {}
+
+BitcoinExchange::BitcoinExchange()
+{
+	std::ifstream file(databasePath.c_str());
+	
+	if (!file.good())
+	{
+		std::cerr << "Error: Database file does not exist." << std::endl;
+		throw std::runtime_error("Database file does not exist.");
+	}
+	loadDatabase(BitcoinExchange::databasePath);
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{
+	m_data = other.m_data;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	if (this != &other)
+	{
+		m_data = other.m_data;
+	}
+	return *this;
+}
+
+void BitcoinExchange::loadDatabase(const std::string &database)
+{
+	std::ifstream file(database.c_str());
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file " << database << std::endl;
+		return;
+	}
+
+	std::string line;
+	bool parse_error = false;
+	int c = 0;
+	std::getline(file, line); // skip header
+	while (std::getline(file, line) && c < 3)
+	{
+		Record r = parseDatabaseLine(line, parse_error);
+		++c;
+		if (!parse_error)
+		{
+			m_data[r.date] = r.value;
+		}
+	}
+	file.close();
+}
+
+BitcoinExchange::Record BitcoinExchange::parseLine(const std::string &raw_line, bool &error, char sep, bool verbose)
+{
+	(void)verbose;
+	std::istringstream ss(raw_line);
+	
+	std::string line;
+	
+	std::string date;
+	float value;
+	
+	//std::cout << "parseLine::raw_line:" << raw_line << "||";
+	if (!std::getline(ss, line, sep))
+	{
+		std::cout << "Error: could not parse line." << std::endl;
+		error = true;
+		return Record("", 0);
+	}
+	std::cout << "date::" << line << "||";
+	date = line;
+	if (!std::getline(ss, line, sep))
+	{
+		std::cout << "Error: could not parse line." << std::endl;
+		error = true;
+		return Record("", 0);
+	}
+	std::cout << "value::" << line << "||";
+	value = std::strtof(line.c_str(), NULL);
+	if (errno == ERANGE)
+	{
+		std::cout << "Error: could not parse value." << std::endl;
+		error = true;
+		return Record("", 0);
+	}
+	
+	
+	return Record(date, value);
+}
+
+BitcoinExchange::Record BitcoinExchange::parseDatabaseLine(const std::string &line, bool &error, bool verbose)
+{
+	return parseLine(line, error, ',', verbose);
+}
+
+BitcoinExchange::Record BitcoinExchange::parseInputLine(const std::string &line, bool &error, bool verbose)
+{
+	return parseLine(line, error, ' ', verbose);
+}
+
+float BitcoinExchange::applyExchangeRate(const std::string &date, float amount) const
+{
+	std::map<std::string, float>::const_iterator it = m_data.find(date);
+	if (it == m_data.end())
+	{
+		std::cerr << "Error: could not find exchange rate for date " << date << std::endl;
+		throw std::runtime_error("Could not find exchange rate for date.");
+	}
+	return it->second * amount;
+}
+
+void BitcoinExchange::applyRatesToFile(const std::string &inputFile)
+{
+	std::ifstream file(inputFile.c_str());
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file " << inputFile << std::endl;
+		throw std::runtime_error("Could not open file.");
+	}
+	
+	std::string line;
+	bool parse_error = false;
+	while (std::getline(file, line))
+	{
+		Record r = parseInputLine(line, parse_error);
+		if (!parse_error)
+		{
+			float result = applyExchangeRate(r.date, r.value);
+			std::cout << r.date << " " << result << std::endl;
+		}
+	}
+	file.close();
+}
+
+bool BitcoinExchange::validDate(const std::string &date) const
+{
+	(void)date;
+	return true;
+}
+
+
+bool BitcoinExchange::validValue(float value) const
+{
+	(void)value;
+	return true;
+}
