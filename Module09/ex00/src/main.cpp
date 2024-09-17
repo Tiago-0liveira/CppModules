66e9b191847e999fe623879bfd1d08ce87cfc6a3
Tiago-0liveira <tfgoliveira@gmail.com>
2024-09-17 23:35:01 +0100

    started cpp09-ex00

---
+++ Module09/ex00/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <BitcoinExchange.hpp>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
+
+	try {
+		BitcoinExchange exchange;
+		
+		exchange.applyRatesToFile(argv[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
